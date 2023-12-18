0ca42ae986239f17d39e83769553067d4cb095d5
taher41 <107751005+taher41@users.noreply.github.com>
2023-12-18 15:58:42 +0300

    Add project files.

---
+++ FoodFactory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodFactory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
