517a7cf6cc6b3fdac006a90624d87ef3b0c1cfbf
Gebarteci <yavuz.kibaroglu@ogr.sakarya.edu.tr>
2023-12-19 19:20:40 +0300

    ..

--- FoodFactory/Controllers/Restaurant.cs
+++ FoodFactory/Controllers/Restaurant.cs
@@ -17,12 +17,12 @@ namespace FoodFactory.Controllers
 
             List<Food> Products = new List<Food>() {
 
-                new Food() {FoodId=0,FoodName="Doner wrap",Price=3.50,Status=true},
-                new Food() {FoodId=1,FoodName="Zurna wrap",Price=5,Status=true},
-                new Food() {FoodId=2,FoodName="Doner",Price=3.50,Status=false},
-                new Food() {FoodId=3,FoodName="Mega Zurna Wrap",Price=7.90,Status=true},
-                new Food() {FoodId=4,FoodName="Ayran",Price=1,Status=true},
-                new Food() {FoodId=5,FoodName="Soda",Price=1,Status=true},
+                new Food() {FoodId=0,FoodName="Doner wrap",Price=3.50,Status=true,Description="Cheese"},
+                new Food() {FoodId=1,FoodName="Zurna wrap",Price=5,Status=true,Description="Cheese"},
+                new Food() {FoodId=2,FoodName="Doner",Price=3.50,Status=false,Description="Cheese"},
+                new Food() {FoodId=3,FoodName="Mega Zurna Wrap",Price=7.90,Status=true,Description="Cheese"},
+                new Food() {FoodId=4,FoodName="Ayran",Price=1,Status=true,Description="Cheese"},
+                new Food() {FoodId=5,FoodName="Soda",Price=1,Status=true,Description="Cheese"},
 
 
             };
@@ -36,12 +36,12 @@ namespace FoodFactory.Controllers
 
             List<Food> Products = new List<Food>() {
 
-                new Food() {FoodId=0,FoodName="Margarita",Price=3.50,Status=true},
-                new Food() {FoodId=1,FoodName="Mixed",Price=5,Status=true},
-                new Food() {FoodId=2,FoodName="Meat Master",Price=3.50,Status=false},
-                new Food() {FoodId=3,FoodName="Vegan",Price=7.90,Status=true},
-                new Food() {FoodId=4,FoodName="Ayran",Price=1,Status=true},
-                new Food() {FoodId=5,FoodName="Soda",Price=1,Status=true},
+                new Food() {FoodId=0,FoodName="Margarita",Price=3.50,Status=true,Description="Cheese"},
+                new Food() {FoodId=1,FoodName="Mixed",Price=5,Status=true,Description="Mixed"},
+                new Food() {FoodId=2,FoodName="Meat Master",Price=3.50,Status=false,Description="To meat lovers"},
+                new Food() {FoodId=3,FoodName="Vegan",Price=7.90,Status=true,Description="Vegan"},
+                new Food() {FoodId=4,FoodName="Ayran",Price=1,Status=true,Description="Classical turk drink."},
+                new Food() {FoodId=5,FoodName="Soda",Price=1,Status=true,Description="Soda"},
 
 
             };
